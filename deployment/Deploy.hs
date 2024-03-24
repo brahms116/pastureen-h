af9636426cb80fa3a269e2f2a6733d6aec145238
David Kwong <20544dk@gmail.com>
2024-03-24 10:08:23 +1000

    Yeah better

--- deployment/Deploy.hs
+++ deployment/Deploy.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 import qualified Control.Concurrent as C
+import Control.Monad.Catch
 import Control.Monad.Reader
 import Data.String
 import qualified Database.PostgreSQL.Simple as PG
@@ -9,38 +15,86 @@ import qualified System.Process as P
 databases :: [String]
 databases = ["nocodb"]
 
-type DatabaseContext a = ReaderT PG.Connection IO a
+class (MonadMask m) => MonadInfrastructure conn m | m -> conn where
+  listDatabases :: conn -> m [String]
+  createDatabase :: conn -> String -> m ()
+  openTunnel :: m conn
+  closeTunnel :: conn -> m ()
+  logMessage :: String -> m ()
+  deployApplication :: m ()
+  deployDatabase :: m ()
+  requiredDatabases :: m [String]
 
-listDatabases :: DatabaseContext [String]
-listDatabases =
-  ask >>= \conn ->
+  bracketTunnel :: (conn -> m a) -> m a
+  bracketTunnel = bracket openTunnel closeTunnel
+
+newtype Config = Config {cfDatabases :: [String]}
+
+-- newtype AppM a = AppM {unApp :: ReaderT Config IO a}
+--   deriving (Functor, Applicative, Monad, MonadIO, MonadThrow, MonadCatch, MonadMask)
+--
+
+type AppM = ReaderT Config IO
+
+instance MonadInfrastructure (P.ProcessHandle, PG.Connection) AppM where
+  logMessage = liftIO . putStrLn
+
+  deployDatabase =
     liftIO $
-      map PG.fromOnly <$> (PG.query_ conn "SELECT datname FROM pg_database" :: IO [PG.Only String])
+      P.callCommand "cd ../infrastructure/local/db && terraform init && terraform apply -auto-approve"
 
-createDatabase :: String -> DatabaseContext ()
-createDatabase name =
-  let statement = fromString $ "CREATE DATABASE " ++ name
-   in ask >>= \conn ->
-        liftIO $ PG.execute_ conn statement >> putStrLn ("Database " ++ name ++ " created")
+  deployApplication =
+    liftIO $
+      P.callCommand "cd ../infrastructure/local/application && terraform init && terraform apply -auto-approve"
 
-syncDatabases :: DatabaseContext ()
-syncDatabases = do
-  existingDatabases <- listDatabases
-  liftIO $ do
-    putStrLn "Syncing databases..."
-    putStrLn "Existing databases:"
-    mapM_ putStrLn $ (" -" ++) <$> existingDatabases
-    putStrLn "Required databases:"
-    mapM_ putStrLn $ (" -" ++) <$> databases
-  mapM_ createDatabase (filter (`notElem` existingDatabases) databases)
+  listDatabases (_, c) =
+    liftIO $
+      map PG.fromOnly <$> (PG.query_ c "SELECT datname FROM pg_database" :: IO [PG.Only String])
+
+  createDatabase (_, c) name =
+    let statement = fromString $ "CREATE DATABASE " ++ name
+     in liftIO $ PG.execute_ c statement >> putStrLn ("Database " ++ name ++ " created")
+
+  openTunnel = do
+    liftIO $ do
+      P.callCommand "kubectl config set-context --current --namespace=pastureen"
+      ph <- P.spawnCommand "kubectl port-forward svc/database 5432:5432"
+      -- Wait 3 seconds for the port-forward to be ready
+      C.threadDelay 3000000
+      conn <- PG.connectPostgreSQL "postgresql://postgres@localhost:5432/postgres"
+      return (ph, conn)
+
+  closeTunnel (ph, _) = liftIO $ P.terminateProcess ph
+
+  requiredDatabases = asks cfDatabases
+
+dbsToCreate :: [String] -> [String] -> ([String], String)
+dbsToCreate existing required =
+  (missing, msg)
+  where
+    missing = filter (`notElem` existing) required
+    fmtDotpoint x = (" -" ++) <$> x
+    msgForCreate = case missing of
+      [] -> []
+      ds -> "Dbs to create: " : fmtDotpoint ds
+    msg =
+      unlines $
+        "Existing dbs"
+          : fmtDotpoint existing
+          ++ "Required dbs: "
+          : fmtDotpoint required
+          ++ msgForCreate
+
+fillMissingDbs :: (MonadInfrastructure conn m) => m ()
+fillMissingDbs =
+  let fill_ c = do
+        dbs <- listDatabases c
+        required <- requiredDatabases
+        let (dbs', report) = dbsToCreate dbs required
+         in do 
+          logMessage report
+          mapM_ (createDatabase c) dbs'
+   in bracketTunnel fill_
 
 main :: IO ()
-main = do
-  P.callCommand "cd ../infrastructure/local/db && terraform init && terraform apply -auto-approve"
-  P.callCommand "kubectl config set-context --current --namespace=pastureen"
-  ph <- P.spawnCommand "kubectl port-forward svc/database 5432:5432"
-  -- Wait 3 seconds for the port-forward to be ready
-  C.threadDelay 3000000
-  conn <- PG.connectPostgreSQL "postgresql://postgres@localhost:5432/postgres"
-  runReaderT syncDatabases conn
-  P.terminateProcess ph
+main = putStrLn "Hello, Haskell!"

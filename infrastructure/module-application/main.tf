405a0484fc1363e34bf35389eb8722bacf6bf04c
David Kwong <20544dk@gmail.com>
2024-04-27 22:35:00 +1000

    Something is working

--- infrastructure/module-application/main.tf
+++ infrastructure/module-application/main.tf
@@ -8,8 +8,42 @@ resource "helm_release" "traefik_proxy" {
   repository = "https://traefik.github.io/charts"
   chart      = "traefik"
   set {
-    name  = "ports.websecure.expose"
-    value = "false"
+    name  = "ports.websecure.expose.default"
+    value = var.environment == "LOCAL" ? "false" : "true"
+  }
+
+  dynamic "set" {
+    for_each = var.environment == "LOCAL" ? [] : [
+      {
+        name  = "ports.websecure.tls.certResolver"
+        value = "letsencrypt"
+      },
+      {
+        name  = "ports.websecure.tls.domains[0].main"
+        value = "noco.davidkwong.net"
+      },
+      {
+        name = "certResolvers.letsencrypt.email"
+        value = "davidkwong17@gmail.com"
+      },
+      {
+        name = "certResolvers.letsencrypt.httpChallenge.entryPoint"
+        value = "web"
+      },
+      {
+        name = "certResolvers.letsencrypt.storage"
+        value = "/data/acme.json"
+      },
+      {
+        name = "service.spec.loadBalancerIP"
+        value = "192.9.182.251"
+      }
+    ]
+
+    content {
+      name  = set.value.name
+      value = set.value.value
+    }
   }
 }
 
@@ -21,7 +55,7 @@ resource "kubernetes_ingress_v1" "noco" {
 
   spec {
     rule {
-      host = "noco.me.davidkwong.net"
+      host = var.environment == "LOCAL" ? "noco.me.davidkwong.net" : "noco.davidkwong.net"
       http {
         path {
           path = "/"

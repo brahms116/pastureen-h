4a913cabbc4a9eef1b2e4e5c9d0dcb10f094600e
David Kwong <20544dk@gmail.com>
2024-05-06 15:28:47 +1000

    Better

--- util-scripts/test/Util.hs
+++ util-scripts/test/Util.hs
@@ -9,7 +9,7 @@ module Util
   ( TestOverrides (..),
     withDockerDatabase,
     runDbTest,
-    getConn
+    getConn,
   )
 where
 
@@ -27,45 +27,55 @@ data TestOverrides = Overrides
     oMigrationDir :: !String
   }
 
-startDatabase :: IO String
-startDatabase = do
+-- | Starts a docker container with a postgres database and returns the container id
+startDockerDatabase :: IO String
+startDockerDatabase = do
   containerId <- P.readProcess "docker" ["run", "-d", "-e", "POSTGRES_PASSWORD=postgres", "-p", "5432:5432", "postgres:13"] ""
   threadDelay 5000000
   return $ init containerId
 
-stopDatabase :: String -> IO ()
-stopDatabase containerId = P.callProcess "docker" ["stop", containerId] >> P.callProcess "docker" ["rm", containerId]
+-- | Stops and removes the database container given its id
+stopDockerDatabase :: String -> IO ()
+stopDockerDatabase containerId =
+  P.callProcess "docker" ["stop", containerId] >> P.callProcess "docker" ["rm", containerId]
 
+-- | Bracket to use with hspec for the around_ hook to start and stop the database container
 withDockerDatabase :: IO () -> IO ()
-withDockerDatabase a = bracket startDatabase stopDatabase $ const a
+withDockerDatabase a = bracket startDockerDatabase stopDockerDatabase $ const a
 
+-- | Fake open tunnel function which just returns the connection to the docker database
 fakeOpenTunnel :: String -> DbEnvironment -> AppM (P.ProcessHandle, PG.Connection)
 fakeOpenTunnel db _ = lift $ do
   conn <- getConn db
   ph <- P.spawnCommand "echo 'Connecting to database'"
   return (ph, conn)
 
+-- | Returns a connection to the local docker database given the database name
 getConn :: String -> IO PG.Connection
 getConn s = PG.connectPostgreSQL $ fromString $ "postgres://postgres:postgres@localhost:5432/" ++ s
 
+-- | Fake close tunnel function which just prints "Closing" as there's nothing to close with
+-- a local docker database
 fakeCloseTunnel :: (P.ProcessHandle, PG.Connection) -> AppM ()
 fakeCloseTunnel _ = lift $ putStrLn "Closing"
 
-pipelineTestOverrides :: String -> TestOverrides
-pipelineTestOverrides dir =
+-- | Returns a TestOverrides given a migration directory fit for database testing
+dbTestOverrides :: String -> TestOverrides
+dbTestOverrides dir =
   Overrides
     { oOpenTunnel = Just fakeOpenTunnel,
       oCloseTunnel = Just fakeCloseTunnel,
       oMigrationDir = dir
     }
 
+-- | Runs a database test given a migration directory
 runDbTest :: ReaderT TestOverrides AppM a -> String -> IO a
 runDbTest m migrationDir =
   runReaderT (runReaderT m ov) cf
   where
     deriveConfig :: TestOverrides -> Config
     deriveConfig x = defaultConfig {cfMigrationDir = oMigrationDir x}
-    ov = pipelineTestOverrides migrationDir
+    ov = dbTestOverrides migrationDir
     cf = deriveConfig ov
 
 instance MonadAbstract (P.ProcessHandle, PG.Connection) (ReaderT TestOverrides AppM) where

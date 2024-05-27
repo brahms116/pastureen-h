a9de864b6c3d5e5411ac73098dce033dca36fcbf
David Kwong <20544dk@gmail.com>
2024-05-27 18:36:12 +1000

    Better docs

--- infrastructure/module-application/main.tf
+++ infrastructure/module-application/main.tf
@@ -74,7 +74,7 @@ resource "helm_release" "traefik_proxy" {
         name  = "certResolvers.letsencrypt.storage"
         value = "/data/acme.json"
       },
-      # We run a single node microk8, so we set the service to nodeport and expose the ports
+      # I run a single node microk8, so I set the service to nodeport and expose the ports
       # directly. I've also hacked the service node port range to allow for this
       {
         name = "service.type"

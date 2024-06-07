88a3c7c1dd1308d1dce54c235c2822109a404ac9
David Kwong <20544dk@gmail.com>
2024-06-08 09:47:18 +1000

    Not yet

--- application/src/Todos/Domain.hs
+++ application/src/Todos/Domain.hs
@@ -8,6 +8,7 @@ module Todos.Domain
     MonadCreateTodo (..),
     MonadDeleteTodo (..),
     MonadGetTodos (..),
+    TodoistToken
   )
 where
 
---
+++ application/worker/Main.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main (main) where
+
+import Control.Monad.Reader
+import qualified Data.Text as T
+import Notifications
+import OverdueTodos
+import System.Environment
+import Todos.Domain
+import Util
+
+data PtTaskName = NotifiyOverdueTodos deriving (Show)
+
+getPtTaskName :: IO PtTaskName
+getPtTaskName = do
+  taskName <- getEnv "PT_TASK_NAME"
+  return $ case taskName of
+    "NOTIFY_OVERDUE_TODOS" -> NotifiyOverdueTodos
+    t -> error $ "Invalid task name: " <> t
+
+data OverdueTodoConfig = OverdueTodoConfig
+  { odtCfgTodoistToken :: !TodoistToken,
+    odtCfgNtfyTopic :: !NotificationTopic
+  }
+
+defaultOdtCfg :: IO OverdueTodoConfig
+defaultOdtCfg = do
+  token <- T.pack <$> getEnv "PT_TODOIST_TOKEN"
+  topic <- T.pack <$> getEnv "PT_NTFY_TOPIC"
+  return $ OverdueTodoConfig token topic
+
+instance HasTodoistToken OverdueTodoConfig where
+  getTodoistToken = odtCfgTodoistToken
+
+instance HasNotificationTopic OverdueTodoConfig where
+  getNotificationTopic = odtCfgNtfyTopic
+
+newtype OverdueTodoAppM a = OverdueTodoAppM
+  { unOverdueTodoAppM :: ReaderT OverdueTodoConfig IO a
+  }
+  deriving newtype
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadIO,
+      MonadReader OverdueTodoConfig
+    )
+  deriving anyclass
+    ( MonadRunHttp,
+      MonadSendNotification,
+      MonadGetTodos,
+      MonadOverdueTodos
+    )
+
+runOverdueTodoTask :: IO ()
+runOverdueTodoTask = do
+  cfg <- defaultOdtCfg
+  runReaderT (unOverdueTodoAppM notifyOverdueTodos) cfg
+
+main :: IO ()
+main = do
+  taskName <- getPtTaskName
+  putStrLn $ "Running task: " ++ show taskName
+  case taskName of
+    NotifiyOverdueTodos -> runOverdueTodoTask
+  putStrLn "Task completed"

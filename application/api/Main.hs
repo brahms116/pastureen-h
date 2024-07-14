31426135657deea6ac881db3ccf726051dfd5c2f
David Kwong <20544dk@gmail.com>
2024-07-14 11:09:10 +1000

    Scaffold complete

--- application/api/Main.hs
+++ application/api/Main.hs
@@ -12,7 +12,7 @@ import Servant.HTML.Blaze
 import qualified Text.Blaze.Html as H
 import Text.Hamlet
 
-type API = Get '[HTML] H.Html
+type API = Get '[HTML] H.Html :<|> "static" :> Raw
 
 withHead :: T.Text -> H.Html -> H.Html
 withHead title body =
@@ -21,13 +21,22 @@ withHead title body =
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Mono&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.3.3/src/css/preflight.css">
+        <link rel="stylesheet" href="/static/styles.css">
         <title>#{title}
       <body>
         ^{body}
   |]
 
 server :: Server API
-server = return $ withHead "Home" [shamlet|<h1>Welcome to my website!|]
+server =
+  let homepage = return $ withHead "Home" [shamlet|<h1>Welcome to my website!|]
+      static = serveDirectoryWebApp "public"
+   in homepage :<|> static
 
 app :: Application
 app = serve (Proxy :: Proxy API) server
